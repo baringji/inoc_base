caadb15ce57f60749e817c7b379b84a9bcc49a83
Jofil Inoc Baring <jofil.baring@gmail.com>
2025-04-05 03:03:41 +0800

    feat: initial commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,25 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "my_module",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+    "name": "Inoc Base Module",
+    "summary": """
+        Inoc Base Module
+        """,
+    "description": """
+        Inoc Base Module
+        Initializes inoc modules and create a settings sub menu. 
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
+    "author": "Jofil Inoc Baring <jofil.baring@gmail.com>",
+    "website": "https://inoc.me",
+    "license": "LGPL-3",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    "category": "Technical",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "depends": ["base", "mail"],
+    "data": [
+        "views/res_config_settings_views.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "application": False,
+    "auto_install": True,
 }
